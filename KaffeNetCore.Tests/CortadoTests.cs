f874fa4fce7464a7f1e9f30a168466259ee90b8b
Fearce <thekev93@gmail.com>
2018-09-11 11:44:05 +0200

    cortadotest
    init test commit

---
+++ KaffeNetCore.Tests/CortadoTests.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KaffeNetCore.Tests
+{
+    [TestClass]
+    public class CortadoTests
+    {
+        [TestMethod]
+        public void CortadoPrisTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert 
+        }
+
+        [TestMethod]
+        public void CortadoMlMælkTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+    }
+}
